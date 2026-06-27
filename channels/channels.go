b3709a4cd35a323a18d3c11ee907a3d89ab0c1fb
grimgrinn <grimgrinn@gmail.com>
2026-06-27 15:05:33 +0300

    concurrency task - mutex

---
+++ channels/channels.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	ch := make(chan int)
+
+	for i := range 5 {
+		go func() {
+			ch <- i + 1
+		}()
+	}
+
+	for range 5 {
+		val := <-ch
+
+		fmt.Println(val)
+	}
+
+}
---
+++ task_conc/task_conc.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"sync"
+	"time"
+)
+
+func randomWait() int {
+	workSeconds := rand.Intn(5 + 1)
+
+	time.Sleep(time.Duration(workSeconds) * time.Second)
+
+	return workSeconds
+}
+
+func main() {
+	wg := &sync.WaitGroup{}
+	locker := &sync.Mutex{}
+	totalWorkSeconds := 0
+
+	start := time.Now()
+
+	wg.Add(100)
+	for range 100 {
+		go func() {
+			defer wg.Done()
+			seconds := randomWait()
+
+			locker.Lock()
+			totalWorkSeconds += seconds
+			locker.Unlock()
+		}()
+	}
+
+	wg.Wait()
+
+	mainSeconds := time.Since(start)
+	fmt.Println("main", mainSeconds)
+	fmt.Println("total", totalWorkSeconds, "seconds")
+}
